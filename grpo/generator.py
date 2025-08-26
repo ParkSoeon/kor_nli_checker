6accf31e28782ffb56c20a18fac7e9b6c24470df
ParkSoeon <106726934+ParkSoeon@users.noreply.github.com>
2025-08-27 04:46:44 +0900

    Update generator.py

--- grpo/generator.py
+++ grpo/generator.py
@@ -2,8 +2,10 @@
 
 import torch
 from tqdm import tqdm
+from typing import List, Dict
+from model import format_input_prompt
 
-def generate_candidates(self, model, tokenizer, input_text, num_candidates=5, max_new_tokens=64, temperature=0.7, top_p=0.95):
+def generate_candidates(model, tokenizer, input_text, num_candidates=5, max_new_tokens=64, temperature=0.7, top_p=0.95, device) -> List[str]:
     model.eval()
     candidates = []
 
@@ -12,9 +14,9 @@ def generate_candidates(self, model, tokenizer, input_text, num_candidates=5, ma
             input_text,
             return_tensors='pt',
             truncation=True,
-            max_length=self.max_length,
+            max_length=512,
         ).to(device)
-        inputs = {k: v.to(model.device) for k, v in inputs.items()}
+        # inputs = {k: v.to(model.device) for k, v in inputs.items()}
 
         for _ in range(num_candidates):
             outputs = model.generate(
@@ -37,13 +39,15 @@ def generate_candidates(self, model, tokenizer, input_text, num_candidates=5, ma
 
     return candidates
 
-def generate_adapter_a_candidates(self, adapter_a, tokenizer, data_smaples, batch_size=1, num_candidates=5):
+def generate_adapter_a_candidates(adapter_a, tokenizer, data_smaples: List[Dict], batch_size=1, num_candidates=5) -> Dict[str, List[str]]:
+    all_candidates = {}
+
     for i in tqdm(range(0, len(data_samples), batch_size), desc="Generating Adapter A Candidates"):
         batch_samples = data_samples[i:i+batch_size]
 
         for sample in batch:
-            input_text = format_input_prompt(sample["premise"], sampele["proposition"])
-            candidates = generate_candidates(self, adapter_a, tokenizer, input_text, num_candidates)
+            input_text = format_input_prompt(sample["premise"], sampele["proposition"], sample["label"])
+            candidates = generate_candidates(adapter_a, tokenizer, input_text, num_candidates, device=device)
 
             key = f"{sample['premise']} ||| {sample['proposition']}"
             all_candidates[key] = candidates
